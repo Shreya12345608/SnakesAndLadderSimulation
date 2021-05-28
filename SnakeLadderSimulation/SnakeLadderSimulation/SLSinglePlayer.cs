81c583712a2f502c887935a7c194d2cd0746d29e
Shreya Malviya <malviyashreya26@gmail.com>
2021-05-28 21:14:12 +0530

    [Shreya]Add: The Player rolls the die to get a number between 1 to 6.

--- SnakeLadderSimulation/SnakeLadderSimulation/SLSinglePlayer.cs
+++ SnakeLadderSimulation/SnakeLadderSimulation/SLSinglePlayer.cs
@@ -9,11 +9,13 @@ namespace SnakeLadderSimulation
     class SLSinglePlayer
     {
         public int StartPoint = 0; //initialize by 0
+        public int RanDice;
         public void StartPlaying()
         {
             Console.WriteLine("Game Started! Good Luck "); //Start Game
-           
-            Console.WriteLine("You are at " + StartPoint); //Main Start Point
+            Random Random = new Random(); //Creating Random Object
+            RanDice = Random.Next(1 , 6); // Rndom value in B/w 1 to 6
+            Console.WriteLine("player Role Dice number is:" + " " + RanDice); // Printing Random Value on b/w 1 to 6
         }
     }
 }
